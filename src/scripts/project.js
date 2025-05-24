791f239069c12c1bb266df18e6049b1a396f193f
Mason-Dino <mason.n.dinosaur@gmail.com>
2025-05-24 18:13:35 -0500

    add a error and starting to work on the backend

--- src/scripts/project.js
+++ src/scripts/project.js
@@ -213,3 +213,21 @@ function closeLinkDisplay() {
     document.getElementById("link-add-button").style = 'display: none';
     document.getElementById("link-icon").innerHTML = '<svg onclick="addLinkDisplay()" class="add" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"/></svg>';
 }
+
+async function addLinkBackend() {
+    link = document.getElementById("link-link").value
+    value = document.getElementById("link-value").value
+
+    console.log(link, value)
+
+    if (!(link === '' || value === '')) {
+
+    }
+
+    else {
+        document.getElementById('link-value-error').style = 'display: flex;'
+        setTimeout(function () {
+            document.getElementById('link-value-error').style = 'display: none;'
+        }, 5000);
+    }
+}
