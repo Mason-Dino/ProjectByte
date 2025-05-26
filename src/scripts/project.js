893ba3f1f9872fd8a9929e2528a81e0da21a47ae
Mason-Dino <mason.n.dinosaur@gmail.com>
2025-05-26 15:01:13 -0500

    starting to work on notes feature

--- src/scripts/project.js
+++ src/scripts/project.js
@@ -99,7 +99,14 @@ async function loadProject() {
 
         else if (result.setup.features[i] === 'notes') {
             projectSpace += `
-            <div style="grid-column-start: ${result.setup.notes.column[0]}; grid-row-start: ${result.setup.notes.row[0]}; grid-column-end: ${result.setup.notes.column[1]}; grid-row-end: ${result.setup.notes.row[1]};">Notes</div>
+            <div style="grid-column-start: ${result.setup.notes.column[0]}; grid-row-start: ${result.setup.notes.row[0]}; grid-column-end: ${result.setup.notes.column[1]}; grid-row-end: ${result.setup.notes.row[1]};">
+                <h3>
+                    Notes
+                    <span style="float: right;" id="notes-icon">
+                        <svg class="add" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M840-680v480q0 33-23.5 56.5T760-120H200q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h480l160 160Zm-80 34L646-760H200v560h560v-446ZM480-240q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35ZM240-560h360v-160H240v160Zm-40-86v446-560 114Z"/></svg>
+                    </span>
+                </h3>
+            </div>
             `
         }
 
