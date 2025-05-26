9fafda76c7566f8c93e4d9377da3faac7d22ba02
Mason-Dino <mason.n.dinosaur@gmail.com>
2025-05-26 16:11:30 -0500

    add functions for the intended function of the notes feature

---
+++ src/scripts/project/notes.js
@@ -0,0 +1,38 @@
+async function saveNotes() {
+    notes = document.getElementById("notes").value;
+
+    result = await window.electronAPI.saveNotes(notes)
+    console.log(result)
+
+    if (result === 200) {
+        document.getElementById('notes-save-success').style = 'display: flex;'
+        setTimeout(function () {
+            document.getElementById('notes-save-success').style = 'display: none;'
+        }, 5000);
+    }
+    else {
+        document.getElementById('notes-save-error').style = 'display: flex;'
+        setTimeout(function () {
+            document.getElementById('notes-save-error').style = 'display: none;'
+        }, 5000);
+    }
+}
+
+function preventTab(event) {
+    if (event.key === "Tab") {
+        console.log("test")
+        event.preventDefault();
+
+        const textarea = event.target;
+        const start = textarea.selectionStart;
+        const end = textarea.selectionEnd;
+
+        // Insert tab character at caret
+        textarea.value = textarea.value.substring(0, start) + "\t" + textarea.value.substring(end);
+
+        // Move caret
+        textarea.selectionStart = textarea.selectionEnd = start + 1;
+    }
+}
+
+setInterval(saveNotes, 60000)
