d570fd5a19c14c0c18026ce82570d4dfe9b64338
Mason-Dino <mason.n.dinosaur@gmail.com>
2025-05-25 20:35:37 -0500

    working on a complete milestone function

--- src/scripts/project/milestone.js
+++ src/scripts/project/milestone.js
@@ -87,3 +87,7 @@ function hideTasks(id) {
     <svg onclick="showTasks('${id}')" xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 -960 960 960" width="18px" fill="#e3e3e3"><path d="M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z"/></svg>
     `
 }
+
+function completeMilestone(id) {
+    
+}
