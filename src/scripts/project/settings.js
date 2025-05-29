7263e172ba4024ab4cd1421334c6808103744e55
Mason-Dino <mason.n.dinosaur@gmail.com>
2025-05-29 15:41:05 -0500

    intergrated error and success codes

--- src/scripts/project/settings.js
+++ src/scripts/project/settings.js
@@ -1,12 +1,12 @@
-function updateInfo() {
+async function updateInfo() {
     projectName = document.getElementById("project-name").value
     icon = document.getElementById("project-icon").value
 
     updateNumber = [null, null]
 
     if (!(icon === "none")) {
-        result = changeIcon()
-        if (result === 200)
+        result = await changeIcon()
+        if (result == 200)
             updateNumber[0] = 200
 
         else
@@ -14,8 +14,8 @@ function updateInfo() {
     }
 
     if (!(projectName === "")) {
-        result = changeName()
-        if (result === 200)
+        result = await changeName()
+        if (result == 200)
             updateNumber[1] = 200
 
         else
@@ -24,28 +24,46 @@ function updateInfo() {
 
     if (updateNumber[0] == updateNumber[1]) {
         if (updateNumber[0] == 200) {
-
+            document.getElementById('update-both').style = 'display: flex;'
+            setTimeout(function () {
+                document.getElementById('update-both').style = 'display: none;'
+            }, 5000);
         }
 
         else if (updateNumber[0] == 404) {
-            
+            document.getElementById('error-both').style = 'display: flex;'
+            setTimeout(function () {
+                document.getElementById('error-both').style = 'display: none;'
+            }, 5000);
         }
     }
     else {
         if (updateNumber[0] == 200) {
-    
+            document.getElementById('update-icon').style = 'display: flex;'
+            setTimeout(function () {
+                document.getElementById('update-icon').style = 'display: none;'
+            }, 5000);
         }
     
         else if (updateNumber[0] == 404) {
-    
+            document.getElementById('error-icon').style = 'display: flex;'
+            setTimeout(function () {
+                document.getElementById('error-icon').style = 'display: none;'
+            }, 5000);
         }
     
         if (updateNumber[1] == 200) {
-    
+            document.getElementById('update-name').style = 'display: flex;'
+            setTimeout(function () {
+                document.getElementById('update-name').style = 'display: none;'
+            }, 5000);
         }
     
         else if (updateNumber[1] == 404) {
-    
+            document.getElementById('error-name').style = 'display: flex;'
+            setTimeout(function () {
+                document.getElementById('error-name').style = 'display: none;'
+            }, 5000);
         }
     }
 }
@@ -60,6 +78,6 @@ async function changeIcon() {
 async function changeName() {
     projectName = document.getElementById("project-name").value
     result = await window.electronAPI.changeName(projectName)
-    
+    console.log(result)
     return result
 }
