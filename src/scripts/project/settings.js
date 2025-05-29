462d915d1cc2ebbec47fbcf231c1a606f7749cbe
Mason-Dino <mason.n.dinosaur@gmail.com>
2025-05-29 18:13:55 -0500

    added id to html elements

--- src/scripts/project/settings.js
+++ src/scripts/project/settings.js
@@ -88,18 +88,18 @@ async function loadSettings() {
     
     for (c = 0; c < result.color.colors.length; c ++ ) {
         document.getElementById("colors").innerHTML += `
-        <div class="hex" style="background-color: ${result.color.colors[c]};"></div>
-        <p>${result.color.colors[c]}</p>
-        <button><svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#e3e3e3"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg></button>
+        <div id="${c}-color-display" class="hex" style="background-color: ${result.color.colors[c]};"></div>
+        <p id="${c}-color-hex">${result.color.colors[c]}</p>
+        <button id="${c}-color-delete"><svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#e3e3e3"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg></button>
         `
     }
 
     for (l = 0; l < result.link.links.length; l ++) {
         document.getElementById("links").innerHTML += `
-        <a href="${result.link.links[l].link}" target="_blank">
+        <a href="${result.link.links[l].link}" target="_blank" id="${l}-link-link">
             <button class="link-value">${result.link.links[l].value}</button>
         </a>
-        <div class="delete-parent">
+        <div class="delete-parent" id="${l}-link-delete">
             <button><svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#e3e3e3"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg></button>
         </div>
         `
@@ -107,8 +107,8 @@ async function loadSettings() {
 
     for (m = 0; m < result.todo.milestones.length; m ++) {
         document.getElementById("milestones").innerHTML += `
-        <p>${result.todo.milestones[m].milestoneName}</p>
-        <button><svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#e3e3e3"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg></button>
+        <p id="${m}-milestone-name">${result.todo.milestones[m].milestoneName}</p>
+        <button id="${m}-milestone-delete"><svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#e3e3e3"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg></button>
         `
     }
 }
