57974d75f609f453869355e111e086a77260e56f
Mason-Dino <mason.n.dinosaur@gmail.com>
2025-05-29 12:43:27 -0500

    added errors

--- src/scripts/project/settings.js
+++ src/scripts/project/settings.js
@@ -2,23 +2,64 @@ function updateInfo() {
     projectName = document.getElementById("project-name").value
     icon = document.getElementById("project-icon").value
 
+    updateNumber = [null, null]
+
     if (!(icon === "none")) {
-        changeIcon()
+        result = changeIcon()
+        if (result === 200)
+            updateNumber[0] = 200
+
+        else
+            updateNumber[0] = 404
     }
 
     if (!(projectName === "")) {
-        changeName()
+        result = changeName()
+        if (result === 200)
+            updateNumber[1] = 200
+
+        else
+            updateNumber[1] = 404
+    }
+
+    if (updateNumber[0] == updateNumber[1]) {
+        if (updateNumber[0] == 200) {
+
+        }
+
+        else if (updateNumber[0] == 404) {
+            
+        }
+    }
+    else {
+        if (updateNumber[0] == 200) {
+    
+        }
+    
+        else if (updateNumber[0] == 404) {
+    
+        }
+    
+        if (updateNumber[1] == 200) {
+    
+        }
+    
+        else if (updateNumber[1] == 404) {
+    
+        }
     }
 }
 
 async function changeIcon() {
     icon = document.getElementById("project-icon").value
-    await window.electronAPI.changeIcon(icon)
-    console.log(icon)
+    result = await window.electronAPI.changeIcon(icon)
+    
+    return result
 }
 
 async function changeName() {
     projectName = document.getElementById("project-name").value
-    await window.electronAPI.changeName(projectName)
-    console.log(projectName)
+    result = await window.electronAPI.changeName(projectName)
+    
+    return result
 }
