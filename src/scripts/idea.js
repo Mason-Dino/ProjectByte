718decf93376bb24c292e703f708e142c3847579
Mason-Dino <mason.n.dinosaur@gmail.com>
2025-06-04 15:14:32 -0500

    intergrated new icon - no function

--- src/scripts/idea.js
+++ src/scripts/idea.js
@@ -9,6 +9,7 @@ async function displayIdea(id) {
 
     document.getElementById(`${id}-add`).style = "display: flex;"
     document.getElementById(`${id}-delete`).style = "display: flex;"
+    document.getElementById(`${id}-edit`).style = "display: flex;"
 
     document.getElementById(`${id}-subidea`).style = "display: block;"
 
@@ -25,6 +26,7 @@ function displayCloseIdea(id) {
 
     document.getElementById(`${id}-add`).style = "display: none;"
     document.getElementById(`${id}-delete`).style = "display: none;"
+    document.getElementById(`${id}-edit`).style = "display: none;"
     document.getElementById(`${id}-subidea`).style = "display: none;"
 }
 
@@ -85,6 +87,9 @@ async function loadIdeas() {
                 <svg id="${id}-arrow-button" onclick="displayIdea('${id}')" xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 -960 960 960" width="18px" fill="#e3e3e3"><path d="M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z"/></svg>
             </span>
             ${result.ideas[i].ideaName}
+            <span id="${id}-edit" style="display: none;">
+                <svg xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 -960 960 960" width="18px" fill="#e3e3e3"><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/></svg>
+            </span>
             <span id="${id}-add" style="display: none;">
                 <svg id="${id}-add-button" onclick="displayMakeSubidea('${id}')" xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 -960 960 960" width="18px" fill="#e3e3e3"><path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"/></svg>
             </span>
@@ -130,6 +135,9 @@ async function makeIdea() {
                 <svg id="${id}-arrow-button" onclick="displayIdea('${id}')" xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 -960 960 960" width="18px" fill="#e3e3e3"><path d="M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z"/></svg>
             </span>
             ${result.content.ideaName}
+            <span id="${id}-edit" style="display: none;">
+                <svg xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 -960 960 960" width="18px" fill="#e3e3e3"><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/></svg>
+            </span>
             <span id="${id}-add" style="display: none;">
                 <svg id="${id}-add-button" onclick="displayMakeSubidea('${id}')" xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 -960 960 960" width="18px" fill="#e3e3e3"><path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"/></svg>
             </span>
