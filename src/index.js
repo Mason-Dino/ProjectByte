f1d0b1b97398919f1d1c12d38cd8c575f084c93b
Mason-Dino <mason.n.dinosaur@gmail.com>
2025-06-03 20:06:52 -0500

    make sure taht idea.json exists and then added idea.js file

--- src/index.js
+++ src/index.js
@@ -76,6 +76,18 @@ app.whenReady().then(() => {
 			}
 		})
 	}
+
+	if (!(fs.existsSync("idea.json"))) {
+		idea = {
+			ideas: []
+		}
+
+		fs.write("idea.json", JSON.stringify(idea, null, 4), (err) => {
+			if (err) {
+				console.log(err)
+			}
+		})
+	}
 	
 	// On OS X it's common to re-create a window in the app when the
 	// dock icon is clicked and there are no other windows open.
