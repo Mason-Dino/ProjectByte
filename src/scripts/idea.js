4401a10a9c899dd5297e682d833e4590f0280f84
Mason-Dino <mason.n.dinosaur@gmail.com>
2025-06-04 17:14:44 -0500

    update the add idea html

--- src/scripts/idea.js
+++ src/scripts/idea.js
@@ -140,17 +140,26 @@ async function makeIdea() {
     
         document.getElementById("idea-display").innerHTML = `
         <p id="${id}-idea">
-            <span id="${id}-arrow">
+            <span id="${id}-arrow" class="center">
                 <svg id="${id}-arrow-button" onclick="displayIdea('${id}')" xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 -960 960 960" width="18px" fill="#e3e3e3"><path d="M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z"/></svg>
             </span>
-            ${result.content.ideaName}
-            <span id="${id}-edit" style="display: none;">
-                <svg xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 -960 960 960" width="18px" fill="#e3e3e3"><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/></svg>
+            <span class="name">
+                <span id="${id}-idea-name">
+                    ${result.content.ideaName}
+                </span>
+                <span id="${id}-idea-edit" style="display: none">
+                    <input type="text" id="${id}-idea-edit-name" onkeydown="editIdeaInput(event, '${id}')">
+                    <button onclick="displayCloseEditIdea('${id}')"><svg xmlns="http://www.w3.org/2000/svg" height="21px" viewBox="0 -960 960 960" width="21px" fill="#e3e3e3"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg></button>
+                    <button onclick="editIdea('${id}')"><svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#e3e3e3"><path d="M840-680v480q0 33-23.5 56.5T760-120H200q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h480l160 160Zm-80 34L646-760H200v560h560v-446ZM480-240q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35ZM240-560h360v-160H240v160Zm-40-86v446-560 114Z"/></svg></button>
+                </span>
             </span>
-            <span id="${id}-add" style="display: none;">
+            <span id="${id}-edit" style="display: none;" class="center">
+                <svg onclick="displayEditIdea('${id}')" xmlns="http://www.w3.org/2000/svg" height="17px" viewBox="0 -960 960 960" width="17px" fill="#e3e3e3"><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/></svg>
+            </span>
+            <span id="${id}-add" style="display: none;" class="center">
                 <svg id="${id}-add-button" onclick="displayMakeSubidea('${id}')" xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 -960 960 960" width="18px" fill="#e3e3e3"><path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"/></svg>
             </span>
-            <span id="${id}-delete" style="display: none;">
+            <span id="${id}-delete" style="display: none;" class="center">
                 <svg onclick="deleteIdea('${id}')" xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 -960 960 960" width="18px" fill="#e3e3e3"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
             </span>
         </p>
