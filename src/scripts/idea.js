e10bf134017eb243859e3eacace032875f302e05
Mason-Dino <mason.n.dinosaur@gmail.com>
2025-06-03 21:16:41 -0500

    added the frontend functions for adding ideas

--- src/scripts/idea.js
+++ src/scripts/idea.js
@@ -19,3 +19,23 @@ function displayCloseIdea(id) {
     document.getElementById(`${id}-add`).style = "display: none;"
     document.getElementById(`${id}-delete`).style = "display: none;"
 }
+
+function displayAddIdea() {
+    document.getElementById("idea-input").style = "display: flex"
+
+    document.getElementById("idea-icon").innerHTML = `
+    <svg id="idea-icon-button" class=add xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>
+    `
+
+    document.getElementById("idea-icon-button").setAttribute("onclick", "displayCloseAddIdea()")
+}
+
+function displayCloseAddIdea() {
+    document.getElementById("idea-input").style = "display: none"
+
+    document.getElementById("idea-icon").innerHTML = `
+    <svg id="idea-icon-button" class="add" xmlns="http://www.w3.org/2000/svg" height="23px" viewBox="0 -960 960 960" width="23px" fill="#e3e3e3"><path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"/></svg>
+    `
+
+    document.getElementById("idea-icon-button").setAttribute("onclick", "displayAddIdea()")
+}
